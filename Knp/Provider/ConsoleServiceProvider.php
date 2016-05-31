71f29d95245003ebc290b0af51d3a02d373c4e80
Marc Steven Plotz <CrimsonLabs@users.noreply.github.com>
2016-05-31 19:27:19 +0200

    Update ConsoleServiceProvider.php
    Revert

--- Knp/Provider/ConsoleServiceProvider.php
+++ Knp/Provider/ConsoleServiceProvider.php
@@ -2,32 +2,32 @@
 
 namespace Knp\Provider;
 
-use Pimple\Container;
-use Pimple\ServiceProviderInterface;
+use Silex\ServiceProviderInterface;
+
 use Silex\Application;
 
 use Knp\Console\Application as ConsoleApplication;
+
 use Knp\Console\ConsoleEvents;
+
 use Knp\Console\ConsoleEvent;
 
 class ConsoleServiceProvider implements ServiceProviderInterface
 {
-    public function register(Container $app)
+    public function register(Application $app)
     {
-        $app['console'] = function() use ($app) {
+        $app['console'] = $app->share(function() use ($app) {
             $application = new ConsoleApplication(
                 $app,
                 $app['console.project_directory'],
                 $app['console.name'],
                 $app['console.version']
             );
-
             $app['dispatcher']->dispatch(ConsoleEvents::INIT, new ConsoleEvent($application));
-
             return $application;
-        };
+        });
     }
-
+    
     public function boot(Application $app)
     {
     }
